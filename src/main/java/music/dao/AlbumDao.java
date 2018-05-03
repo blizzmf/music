7cb9e49d101d8649cdf5562ed369cab0c5163deb
Олег Королько <38949266+AlehKoralko@users.noreply.github.com>
2018-05-03 19:56:40 +0300

    ss
    ss

--- src/main/java/music/dao/AlbumDao.java
+++ src/main/java/music/dao/AlbumDao.java
@@ -5,7 +5,7 @@ import music.model.Artist;
 import java.util.List;
 
 public interface AlbumDao extends CrudDao<Album>{
- 	kek
+ 	keksess
 	List<Album> findAll();
 	
 	List<Album> getByBand(int fkBand);
